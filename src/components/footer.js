e4bd73c70c6d2845aca94a450203f5506acec026
Brian Jenkins <studiojenkins@yahoo.com>
2021-05-19 18:40:51 -0400

    created footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,28 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-const Footer = () => (
-  <footer className="bg-light p-3 text-center">
-    <div className="logo" />
-    <p>
-      Sample project provided by{" "}
-      <a target="_blank" rel="noopener noreferrer" href="https://auth0.com">
-        Auth0
-      </a>
-    </p>
-  </footer>
-);
+
+function Footer() {
+
+    return (
+        <footer className="mt-5">
+            <Container fluid={true}>
+                <Row className="border-top justify-content-between p-3">
+                    <Col className="p-0" md={3} sm={12}>
+                    Healthy U 
+                    </Col>
+                    <Col className="p=0 d-flex justify-content-end" md={3}>
+                    Encouraging Better Health
+                    </Col>
+
+                </Row>
+            </Container>
+        </footer>
+
+    )
+}
+
 
 export default Footer;
