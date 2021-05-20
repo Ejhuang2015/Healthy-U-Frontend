84f6218227cfa41ce5ee565956385152d6cff8f9
Matthew Romano <mathiasrok@gmail.com>
2021-05-19 20:09:52 -0400

    footer added

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import HomeContent from "./home-content";
-import Footer from "./footer";
+import Footer from "./Footer/footer";
 import Hero from "./hero";
 import Loading from "./loading";
 import NavBar from "./nav-bar";
