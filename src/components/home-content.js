4f3ef638ac5110b0c2b08b0bc6b26360f4c77601
Matthew Romano <74730908+MRomano84@users.noreply.github.com>
2021-05-22 11:39:56 -0400

    homepage content

--- src/components/home-content.js
+++ src/components/home-content.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomeContent = () => (
   <div className="next-steps">
-    <h2 className="my-5 text-center">What can I do next?</h2>
+    <h2 className="my-5 text-center">Build A Healthy You!</h2>
 
     <div className="row">
       <div className="col-md-5 mb-4">
@@ -10,16 +10,16 @@ const HomeContent = () => (
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href="https://auth0.com/docs/connections"
+            href="https://symboliamag.com/3-benefits-of-using-a-habit-tracker/"
           >
-            <i className="fas fa-link mr-2" />
-            Configure other identity providers
+            <i className="fas fa-user-edit mr-2" />
+            Track Your Habits!
           </a>
         </h6>
         <p>
-          Auth0 supports social providers as Facebook, Twitter, Instagram and
-          100+, Enterprise providers as Microsoft Office 365, Google Apps,
-          Azure, and more. You can also use any OAuth2 Authorization Server.
+          Tracking your daily habits will help reinforce and solidify good habits.
+          This is a valuable tool to measure your progress and allows you to see
+          and visualize your success! Great for your physical and mental health!
         </p>
       </div>
 
@@ -30,17 +30,16 @@ const HomeContent = () => (
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href="https://auth0.com/docs/multifactor-authentication"
+            href="https://www.cdc.gov/nutrition/data-statistics/plain-water-the-healthier-choice.html"
           >
-            <i className="fas fa-link mr-2" />
-            Enable Multi-Factor Authentication
+            <i className="fas fa-faucet mr-2" />
+            Drink Your Water!
           </a>
         </h6>
         <p>
-          Add an extra layer of security by enabling Multi-factor
-          Authentication, requiring your users to provide more than one piece of
-          identifying information. Push notifications, authenticator apps, SMS,
-          and DUO Security are supported.
+          Drinking the recommended daily ammount of water has many advantages and is 
+          important for your health. Not only will drinking water prevent dehydration,
+          but it can affect your mood and even digestive health. Drink your water!
         </p>
       </div>
     </div>
@@ -51,16 +50,17 @@ const HomeContent = () => (
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href="https://auth0.com/docs/anomaly-detection"
+            href="https://www.heart.org/en/healthy-living/healthy-eating/add-color/fruits-and-vegetables-serving-sizes"
           >
-            <i className="fas fa-link mr-2" />
-            Anomaly Detection
+            <i className="fas fa-carrot mr-2" />
+            Eat Fruits and Veggies Daily!
           </a>
         </h6>
         <p>
-          Auth0 can detect anomalies and stop malicious attempts to access your
-          application. Anomaly detection can alert you and your users of
-          suspicious activity, as well as block further login attempts.
+          Eating fruits and vegetables daily is import for your body to get
+          the vitamins and nutrients it needs. These vitamins and nutrients 
+          will help your body operate at max levels and will help you feel
+          great!
         </p>
       </div>
 
@@ -71,17 +71,17 @@ const HomeContent = () => (
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href="https://auth0.com/docs/rules"
+            href="https://fitbuddha.com/tips-for-building-sustainable-habits/"
           >
-            <i className="fas fa-link mr-2" />
-            Learn About Rules
+            <i className="fas fa-fist-raised mr-2" />
+            Build Healthy and Sustainable Habits!
           </a>
         </h6>
         <p>
-          Rules are JavaScript functions that execute when a user authenticates
-          to your application. They run once the authentication process is
-          complete, and you can use them to customize and extend Auth0's
-          capabilities.
+          Having consistency with healthy choices can be very hard. And approaching anything
+          with an 'all or nothing' mindset will often set you up for failure. Being accountable 
+          and establishing realistic expectations are a great place to start building sustainable
+          habits. You can do it!
         </p>
       </div>
     </div>
