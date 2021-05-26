28ea64cd894d8218eb32dfe0ce0520ebf0831fa9
Wanda Gleason <oglissane@gmail.com>
2021-05-25 23:46:38 -0400

    Exporting habit cards.

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,18 @@ import Hero from "./hero";
 import Loading from "./loading";
 import NavBar from "./nav-bar";
 import HabitChoices from "./HabitChoices";
+import VeggiesCard from "./VeggiesCard";
+import MeditationCard from "./MeditationCard";
+import HydrationCard from "./HydrationCard";
 
-export { HomeContent, Footer, Hero, Loading, NavBar, HabitChoices };
+export {
+  HomeContent,
+  Footer,
+  Hero,
+  Loading,
+  NavBar,
+  HabitChoices,
+  VeggiesCard,
+  MeditationCard,
+  HydrationCard,
+};
