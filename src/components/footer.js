3dbafbcb45edf06e8257c92f0b2e78253afe3125
Matthew Romano <74730908+MRomano84@users.noreply.github.com>
2021-05-30 17:56:13 -0400

    footer & nav styling match

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,31 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './footer.css';
 
 
 function Footer() {
 
     return (
-        <footer className="mt-5">
-            <Container fluid={true}>
-                <Row className="border-top justify-content-between p-3">
-                    <Col className="p-0 d-flex justify-content-start">
-                    Healthy U 
+        <footer className="mt-5 footer">
+            {/* <Container fluid={true}> */}
+                <Row lg="auto" md="auto" sm="auto" className="justify-content-between">
+                    {/* <Col className="p-0 d-flex justify-content-start"> */}
+                    <Col xs={4} md={4} lg={4}>
+                        Healthy U 
                     </Col>
-                    <Col className="p=0 d-flex justify-content-end">
-                    Encouraging Better Health
+                    {/* <Col className="p-0 d-flex justify-content-center"> */}
+                    <Col xs={4} md={4} lg={4}>
+                        {/* <img alt="logo" src="./images/healthy_u_logo22.png"></img> */}
+                        <Image src="./images/healthy_u_logo22.png" alt="logo" fluid="false" />
                     </Col>
-
+                    {/* // <Col className="p=0 d-flex justify-content-end"> */}
+                    <Col xs={4} md={4} lg={4}>
+                        Encouraging Better Health
+                    </Col>
                 </Row>
-            </Container>
+            {/* </Container> */}
         </footer>
 
     )
